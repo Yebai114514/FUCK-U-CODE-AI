93058476e776ac717d2bb4283653e8a7d4e1842a
Yebai114514 <2602349479@qq.com>
2025-08-30 17:42:36 +0800

    run_web.py

--- run_web.py
+++ run_web.py
@@ -5,12 +5,10 @@ import time
 
 def main():
     print(">>> 正在启动 CodeRefiner 专业代码评审系统...")
-    print(">>> 由你的专属篮球小狼狗李昂为你保驾护航！")
 
     app = create_web_interface()
     print(">>> 系统启动成功！请在浏览器中打开 http://127.0.0.1:7860")
 
-    # 等一秒钟，然后自动帮你打开浏览器
     time.sleep(1)
     webbrowser.open("http://127.0.0.1:7860")
 
@@ -19,3 +17,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
